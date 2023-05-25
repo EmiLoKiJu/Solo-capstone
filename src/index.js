92467051e70a4b919cec88e6e6275c878db11e34
unknown <gabrielrozas1996@gmail.com>
2023-05-24 22:43:21 -0800

    Added a popup function for comments

--- src/index.js
+++ src/index.js
@@ -21,9 +21,34 @@ const buttonclicklike = (itemid) => {
   updatelike(itemid);
 };
 
+const buttonclickcomment = (item) => {
+  console.log('calling button function with: ', item);
+  const popup = document.createElement('div');
+  popup.classList.add('dflex', 'flexcol', 'popupwindow');
+  popup.innerHTML = `
+  <button class="closepopup">X</button>
+  <img class="width500px" src=${item.album.images[0].url}></img>
+  <h1>${item.name}</h1>
+  <div class="dflex spacebetween">
+    <h4>Artist: ${item.artists[0].name}</h4>
+    <h4>Album: ${item.album.name}</h4>
+  </div>
+  <div class="dflex spacebetween">
+    <h4>Listen on Spotify: <a href="${item.external_urls.spotify}" target="_blank">Link</a></h4>
+    <h4>Release date: ${item.album.release_date}</h4>
+  </div>
+  `;
+  document.querySelector('body').appendChild(popup);
+  const closepopup = popup.querySelector('.closepopup');
+  closepopup.addEventListener('click', () => {
+    document.querySelector('body').removeChild(popup);
+  });
+}
+
 const render = async () => {
   const data = await getdata();
   const updatedLikes = await likesforthissearch(data, getLikes());
+  console.log(data.tracks.items);
   for (let i = 0; i < data.tracks.items.length; i += 1) {
     const text = document.createElement('div');
     text.classList.add('songelementcontainer', 'dflex', 'flexcol');
@@ -37,11 +62,16 @@ const render = async () => {
     <div class="dflex likescontainer">
       <button class="likebutton">like</button>
       <p class="likes" id="${data.tracks.items[i].id}">${updatedLikes[i].likes}</p>
-    </div>`;
+    </div>
+    <button class="commentbutton">comment</button>`;
     musiccontainer.appendChild(text);
     const likebutton = text.querySelector('.likebutton');
     likebutton.addEventListener('click', () => {
       buttonclicklike(data.tracks.items[i].id);
+    });
+    const commentbutton = text.querySelector('.commentbutton');
+    commentbutton.addEventListener('click', () => {
+      buttonclickcomment(data.tracks.items[i]);
     });
   }
 };
