d39fcd825f8cb4e031c9fe024b6af1d441726c31
unknown <gabrielrozas1996@gmail.com>
2023-05-24 21:53:07 -0800

    Add footer

--- src/modules/getdata.js
+++ src/modules/getdata.js
@@ -1,5 +1,5 @@
 const artistName = 'nothing but thieves';
-const token = 'BQAhsPzxia1GK8GHsHhrUwx8a-thVkdJNXRoAmDnS8P71uqu8skr_xadUq07tZEkdjcs2NS4RyOfPDFfxq7jjA5NffhkhrUJkaYteFlmuX4QKCayPCRN6LFmOisjsMubBMoUmkJ6iJpfEUzEvQvFWPrkP36NolP2xlSLaMqCeR3gDO51vh7FHCeZnrpnVnJ8Uv_tDJSdN6YM9sE6R6oh67ww-58tnfrDa7zmuSQWhcW4oPPi-ICEr9MLD-NN0REvj0y4lQx48O9PDpV5I26X';
+const token = 'BQDE2NDURN3bZfp_lwxLN9CF6EEpkc0-Fq5PS84AfJJYAyFSitJoHHFlWMyVDQMygzw2Ox5PBFxbwcwpBnzTGfTEUojPWS6FREcrizsGA1K-mvy83OT_5SXZk2oRVWzchPYE1Z65YTHmJWUnOHDMEWR68U26QpnSsAZfHQYt8imyRR4csDkPxef3IGfN_jqJLEa5U8QUm8Fm91qNFEpa4HFSIgkh180eVecPmdcgrp3nrzTsufM5jpfqRsBh2JfTVUy7z09TGhyInTPT0knh';
 
 const getdata = async () => {
   const response = await fetch(`https://api.spotify.com/v1/search?q=${artistName}&type=track`, {
