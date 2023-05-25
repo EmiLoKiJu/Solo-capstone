0341cadf0cc7b57d7fd19e89f05d4c5be32839a6
unknown <gabrielrozas1996@gmail.com>
2023-05-24 21:35:50 -0800

    Add test for 20 elements

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -4,7 +4,7 @@ const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 describe('songelementcounter', () => {
-  test('counting', () => {
+  test('counting 0 elements', () => {
     // Arrange
     const dom = new JSDOM(`
     <html>
@@ -19,6 +19,64 @@ describe('songelementcounter', () => {
 
     // Assert
     expect(result).toBe(0);
+  });
+  test('counting 20 elements', () => {
+    // Arrange
+    const dom = new JSDOM(`
+    <html>
+      <body>
+        <div class="musiccontainer">
+          <div class="songelementcontainer">
+          </div>
+          <div class="songelementcontainer">
+          </div>
+          <div class="songelementcontainer">
+          </div>
+          <div class="songelementcontainer">
+          </div>
+          <div class="songelementcontainer">
+          </div>
+          <div class="songelementcontainer">
+          </div>
+          <div class="songelementcontainer">
+          </div>
+          <div class="songelementcontainer">
+          </div>
+          <div class="songelementcontainer">
+          </div>
+          <div class="songelementcontainer">
+          </div>
+          <div class="songelementcontainer">
+          </div>
+          <div class="songelementcontainer">
+          </div>
+          <div class="songelementcontainer">
+          </div>
+          <div class="songelementcontainer">
+          </div>
+          <div class="songelementcontainer">
+          </div>
+          <div class="songelementcontainer">
+          </div>
+          <div class="songelementcontainer">
+          </div>
+          <div class="songelementcontainer">
+          </div>
+          <div class="songelementcontainer">
+          </div>
+          <div class="songelementcontainer">
+          </div>
+        </div>
+      </body>
+    </html>`);
+    global.document = dom.window.document;
+    const musiccontainer = document.querySelector('.musiccontainer');
+
+    // Act
+    const result = songelementcounter(musiccontainer);
+
+    // Assert
+    expect(result).toBe(20);
   });
 });
 
