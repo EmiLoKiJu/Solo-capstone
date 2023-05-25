ef767b5ac120ddca30cba70e1bb8e9a5a8231d6c
unknown <gabrielrozas1996@gmail.com>
2023-05-24 19:14:57 -0800

    Fix linters for real

--- src/modules/givelikes.js
+++ src/modules/givelikes.js
@@ -1,5 +1,5 @@
 const giveLikes = async (itemidd) => {
-  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/N1YfreMOcnHDHjcZrEgf/likes/', {
+  await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/N1YfreMOcnHDHjcZrEgf/likes/', {
     method: 'POST',
     body: JSON.stringify({
       item_id: `${itemidd}`,
